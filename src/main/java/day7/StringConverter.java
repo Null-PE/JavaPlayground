311cc2bcbff8c0c03fd3a7db76147d007dde6b8f
autotaker <autotaker@gmail.com>
2020-09-09 22:40:57 +0900

    add task

---
+++ src/main/java/day7/StringConverter.java
@@ -0,0 +1,18 @@
+package day7;
+
+public class StringConverter {
+
+    /**
+     * 引数に含まれる[a-z0-9]以外の文字を置換してください
+     * 仕様1: [a-z0-9]の文字は置換しないでください
+     * 仕様2: 空白文字はまとめてハイフン（-）に置換してください(例: @{code: "hoge  \nfuga"}は @{code: "hoge-fuga"})
+     * 仕様3: 大文字は小文字にしてください
+     * 仕様4: それ以外の文字は１文字ずつアンダースコア(_)に置換してください
+     * @param name
+     * @return @{code: "[a-z0-9_-]*"}のみを含む文字列
+     */
+    public String sanitizeName(String name) {
+        // TODO
+        return name;
+    }
+}
