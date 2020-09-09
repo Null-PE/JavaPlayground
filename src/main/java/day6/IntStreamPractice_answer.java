213ea6ae7dd3352ba9db36c357a82f52b3d6e2c8
NULLPE <1989leosun@gmail.com>
2020-09-09 11:04:29 +0900

    day6 answer

--- src/main/java/day6/IntStreamPractice_answer.java
+++ src/main/java/day6/IntStreamPractice_answer.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-public class IntStreamPractice {
+public class IntStreamPractice_answer {
     public static void main(String[] args) {
         /*
         事前知識：IntStreamをゲットする方法
@@ -16,35 +16,34 @@ public class IntStreamPractice {
         IntStream.generate(() -> (int)(Math.random()*100)).limit(5).forEach(System.out::println);
         Arrays.stream("1,2,3,4,5".split(",")).mapToInt(Integer::parseInt).forEach(System.out::println);
          */
-        IntStream.range(1,10).forEach(System.out::println);
-        IntStream.of(1,2,3,56,6,7,1).forEach(System.out::println);
-        IntStream.iterate(1, i -> i * 3).limit(10).forEach(System.out::println);
-        IntStream.generate(() -> (int)(Math.random()*100)).limit(10).forEach(System.out::println);
-        Arrays.stream("1,2,3,4,5".split(",")).mapToInt(Integer::parseInt).forEach(System.out::println);
 
         /*
         課題１ 標準出力System.out.println()を使って、1から100までカウント
          */
-
+        IntStream.range(1,101).forEach(System.out::println);
         /*
         課題２ 50以上100以下3の倍数を配列int[]に保存する
          */
+        int[] arr = IntStream.range(50, 100).filter(i -> i % 3 == 0).toArray();
 
         /*
         課題３ 文字列strに入っている数字の合計を取得
          */
         String str = "5,2,10,50,16,1,20";
+        int sum = Arrays.stream(str.split(",")).mapToInt(Integer::parseInt).sum();
 
         /*
         課題４　社員リストから平均年齢を計算する
          */
         List<Employee> EmployeeList = getEmployeeList();
+        EmployeeList.stream().mapToInt(Employee::getAge).average().ifPresent(System.out::println);
 
         /*
          最終問題：配列aとbを合併して、重複なし+昇順で出力する
          */
         int[] a = {10, 5, 31, 9, 5};
         int[] b = {2, 32, 5, 15, 9};
+        Stream.of(a,b).flatMapToInt(IntStream::of).distinct().sorted().forEach(System.out::println);
 
     }
 
