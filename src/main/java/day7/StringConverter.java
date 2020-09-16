d14b90f63eb117b1b0192fa9965f889021a609b6
autotaker <autotaker@gmail.com>
2020-09-16 21:27:04 +0900

    apply formatter

--- src/main/java/day7/StringConverter.java
+++ src/main/java/day7/StringConverter.java
@@ -2,36 +2,36 @@ package day7;
 
 public class StringConverter {
 
-    /**
-     * 引数に含まれる[a-z0-9]以外の文字を置換してください
-     * 仕様1: [a-z0-9]の文字は置換しないでください
-     * 仕様2: 空白文字はまとめてハイフン（-）に置換してください
-     *       (例: @{code: "hoge  \nfuga"}は @{code: "hoge-fuga"})
-     * 仕様3: 大文字は小文字にしてください
-     * 仕様4: それ以外の文字は１文字ずつアンダースコア(_)に置換してください
-     * @param name
-     * @return @{code: "[a-z0-9_-]*"}のみを含む文字列
-     */
-    public String sanitizeName(String name) {
-    	StringBuilder sb = new StringBuilder();
-    	boolean prevCharIsSpace = false;;
-    	for(int i = 0; i < name.length(); i++) {
-    		char current = name.charAt(i);
-    		if( '0' <= current && current <= '9') {
-    			sb.append(current);
-    		}else if( 'a' <= current && current <= 'z' ) {
-    			sb.append(current);
-    		}else if( 'A' <= current && current <= 'Z' ) {
-    			sb.append(Character.toLowerCase(current));
-    		}else if( Character.isWhitespace(current) ) {
-    			if( !prevCharIsSpace) {
+	/**
+	 * 引数に含まれる[a-z0-9]以外の文字を置換してください
+	 * 仕様1: [a-z0-9]の文字は置換しないでください
+	 * 仕様2: 空白文字はまとめてハイフン（-）に置換してください
+	 *       (例: @{code: "hoge  \nfuga"}は @{code: "hoge-fuga"})
+	 * 仕様3: 大文字は小文字にしてください
+	 * 仕様4: それ以外の文字は１文字ずつアンダースコア(_)に置換してください
+	 * @param name
+	 * @return @{code: "[a-z0-9_-]*"}のみを含む文字列
+	 */
+	public String sanitizeName(String name) {
+		StringBuilder sb = new StringBuilder();
+		boolean prevCharIsSpace = false;
+		for (int i = 0; i < name.length(); i++) {
+			char current = name.charAt(i);
+			if ('0' <= current && current <= '9') {
+				sb.append(current);
+			} else if ('a' <= current && current <= 'z') {
+				sb.append(current);
+			} else if ('A' <= current && current <= 'Z') {
+				sb.append(Character.toLowerCase(current));
+			} else if (Character.isWhitespace(current)) {
+				if (!prevCharIsSpace) {
 					sb.append('-');
-    			}
-    		}else {
-    			sb.append('_');
-    		}
+				}
+			} else {
+				sb.append('_');
+			}
 			prevCharIsSpace = Character.isWhitespace(current);
-    	}
-        return sb.toString();
-    }
+		}
+		return sb.toString();
+	}
 }
--- src/test/java/day7/StringConverterTest.java
+++ src/test/java/day7/StringConverterTest.java
@@ -6,39 +6,39 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class StringConverterTest {
-    StringConverter it;
+	StringConverter it;
 
-    @Before
-    public void setUp() throws Exception {
-        it = new StringConverter();
-    }
+	@Before
+	public void setUp() throws Exception {
+		it = new StringConverter();
+	}
 
-    @Test
-    public void sanitizeName_ReturnAsIs_IfArgumentConsistsOfSmallAlphaNumericChars() {
-        String actual = it.sanitizeName("atoz0to9");
-        String expected = "atoz0to9";
-        assertEquals(expected, actual);
-    }
+	@Test
+	public void sanitizeName_ReturnAsIs_IfArgumentConsistsOfSmallAlphaNumericChars() {
+		String actual = it.sanitizeName("atoz0to9");
+		String expected = "atoz0to9";
+		assertEquals(expected, actual);
+	}
 
-    @Test
-    public void sanitizeName_ReplaceWithLowerCase_IfArgumentContainsUpperCaseChars() {
-        String actual = it.sanitizeName("AtoZ");
-        String expected = "atoz";
-        assertEquals(expected, actual);
-    }
+	@Test
+	public void sanitizeName_ReplaceWithLowerCase_IfArgumentContainsUpperCaseChars() {
+		String actual = it.sanitizeName("AtoZ");
+		String expected = "atoz";
+		assertEquals(expected, actual);
+	}
 
-    @Test
-    public void sanitizeName_ReplaceWithHyphen_IfArgumentContainsContinuousWhitespaces() {
-        String actual = it.sanitizeName("hoge \n\tfuga piyo");
-        String expected = "hoge-fuga-piyo";
-        assertEquals(expected, actual);
-    }
+	@Test
+	public void sanitizeName_ReplaceWithHyphen_IfArgumentContainsContinuousWhitespaces() {
+		String actual = it.sanitizeName("hoge \n\tfuga piyo");
+		String expected = "hoge-fuga-piyo";
+		assertEquals(expected, actual);
+	}
 
-    @Test
-    public void sanitizeName_ReplaceWithUnderscores_IfArgumentContainsOtherChars() {
-        String actual = it.sanitizeName("japane$e @t 日本語");
-        String expected = "japane_e-_t-___";
-        assertEquals(expected, actual);
-    }
+	@Test
+	public void sanitizeName_ReplaceWithUnderscores_IfArgumentContainsOtherChars() {
+		String actual = it.sanitizeName("japane$e @t 日本語");
+		String expected = "japane_e-_t-___";
+		assertEquals(expected, actual);
+	}
 
 }
