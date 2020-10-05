142d21013a69174d9b2c2214d87cd2cf34cb48eb
autotaker <autotaker@gmail.com>
2020-10-05 12:28:37 +0900

    use hamcrest matchers

--- src/test/java/day20/StringPractiseTest.java
+++ src/test/java/day20/StringPractiseTest.java
@@ -11,8 +11,6 @@ import java.util.stream.Stream;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.google.common.collect.Lists;
-
 public class StringPractiseTest {
 	IStringPractise it;
 	@Before
@@ -24,17 +22,14 @@ public class StringPractiseTest {
 	public void splitByComma_引数で渡された文字列をカンマで分割してStringの配列で返す() {
 		String input = "suzuki,sho";
 		String[] actual = it.splitByComma(input);
-		String[] expected = {"suzuki", "sho"};
-		assertArrayEquals(expected,  actual);
-		
+		assertThat(actual, is(arrayContaining("suzuki","sho")));
 	}
 
 	@Test
 	public void splitByDot_引数で渡された文字列をドットで分割してStringの配列で返す() {
 		String input = "suzuki.sho";
 		String[] actual = it.splitByDot(input);
-		String[] expected = {"suzuki", "sho"};
-		assertArrayEquals(expected,  actual);
+		assertThat(actual, is(arrayContaining("suzuki","sho")));
 	}
 
 	@Test
@@ -42,8 +37,7 @@ public class StringPractiseTest {
 		String input = "suzuki_sho";
 		String separator = "_";
 		String[] actual = it.splitByAny(input, separator);
-		String[] expected = {"suzuki", "sho"};
-		assertArrayEquals(expected,  actual);
+		assertThat(actual, is(arrayContaining("suzuki","sho")));
 	}
 
 	@Test
@@ -51,8 +45,7 @@ public class StringPractiseTest {
 		String input = "suzuki.sho";
 		String separator = ".";
 		String[] actual = it.splitByAny(input, separator);
-		String[] expected = {"suzuki", "sho"};
-		assertArrayEquals(expected,  actual);
+		assertThat(actual, is(arrayContaining("suzuki","sho")));
 	}
 
 	@Test
@@ -79,8 +72,7 @@ public class StringPractiseTest {
 	public void splitDayTime_日時を分割してStringの配列で返す() {
 		String input = "2020/09/01 10:30:30";
 		String[] actual = it.splitDayTime(input);
-		String[] expected = {"2020", "09", "01", "10", "30", "30"};
-		assertArrayEquals(expected,  actual);
+		assertThat(actual, is(arrayContaining("2020","09","01","10","30","30")));
 	}
 
 	@Test(expected = IllegalArgumentException.class)
@@ -100,8 +92,7 @@ public class StringPractiseTest {
 		String input = "suzuki.sho";
 		String separator = ".";
 		List<String> actual = it.splitByAnyAsList(input, separator);
-		List<String> expected = Lists.newArrayList("suzuki", "sho");
-		assertThat(expected, is(actual));
+		assertThat(actual, contains("suzuki","sho"));
 	}
 
 	//うまい書き方募集
@@ -110,23 +101,20 @@ public class StringPractiseTest {
 		String input = "suzuki.sho";
 		String separator = ".";
 		Stream<String> actual = it.splitByAnyAsStream(input, separator);
-		List<String> expected = Lists.newArrayList("suzuki", "sho");
-		assertThat(expected, is(actual.collect(Collectors.toList())));
+		assertThat(actual.collect(Collectors.toList()), contains("suzuki","sho"));
 	}
 
 	@Test
 	public void convertToPascalFromSnakeTest_familyName_firstNameをパスカルケースに変換する() {
 		String input = "suzuki_shotaro";
 		String actual = it.convertToPascalFromSnake(input);
-		String expected = "SuzukiShotaro";
-		assertThat(expected, is(actual));
+		assertThat(actual, is("SuzukiShotaro"));
 	}
 
 	@Test
 	public void convertToSnakeFromPascal_familyNameFirstNameをパスカルケースに変換する() {
 		String input = "suzukiShotaro";
 		String actual = it.convertToSnakeFromPascal(input);
-		String expected = "suzuki_shotaro";
-		assertThat(expected, is(actual));
+		assertThat(actual, is("suzuki_shotaro"));
 	}
 }
