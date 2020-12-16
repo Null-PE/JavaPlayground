1d53012a03aeb7b9c58b815dfe1618ad0681a8b6
gengen0719 <happyhappy.style.star@gmail.com>
2020-12-16 17:06:39 +0900

    2020/12/15実装分

--- doc/DobbleGame/app.js
+++ doc/DobbleGame/app.js
@@ -18,26 +18,55 @@ function pickUpAndRemoveRandomImages(targetImageRersourceArray){
     return returnArray;
 }
 
+function getImageResources() {
+    return [{'src':'img/black-circle.png'},
+    {'src':'img/black-square.png'},
+    {'src':'img/black-triangle.png'},
+    {'src':'img/red-circle.png'},
+    {'src':'img/red-square.png'},
+    {'src':'img/red-triangle.png'},
+    {'src':'img/yellow-circle.png'},
+    {'src':'img/yellow-square.png'},
+    {'src':'img/yellow-triangle.png'},
+    {'src':'img/green-circle.png'},
+    {'src':'img/green-square.png'},
+    {'src':'img/green-triangle.png'},
+    {'src':'img/blue-circle.png'},
+    {'src':'img/blue-square.png'},
+    {'src':'img/blue-triangle.png'}
+    ];
+}
+    
+    let $card1 = $("#card1");
+    let $card2 = $("#card2");
 
-let imageResources =  [{'src':'img/black-circle.png'},
-{'src':'img/black-square.png'},
-{'src':'img/black-triangle.png'},
-{'src':'img/red-circle.png'},
-{'src':'img/red-square.png'},
-{'src':'img/red-triangle.png'},
-{'src':'img/yellow-circle.png'},
-{'src':'img/yellow-square.png'},
-{'src':'img/yellow-triangle.png'},
-{'src':'img/green-circle.png'},
-{'src':'img/green-square.png'},
-{'src':'img/green-triangle.png'},
-{'src':'img/blue-circle.png'},
-{'src':'img/blue-square.png'},
-{'src':'img/blue-triangle.png'}
-];
+    function startGame(){
+        let imageResources = getImageResources();
+        
+        let card1Images = pickUpAndRemoveRandomImages(imageResources);
+        let card2Images = pickUpAndRemoveRandomImages(imageResources)
+        
+        let answerIndex = Math.floor(Math.random() * imageResources.length);
+        let answerImage = imageResources[answerIndex];
+        answerImage['class'] = 'answer';
+        
+        let card1AnswerIndex = Math.floor(Math.random() * card1Images.length + 1);
+        card1Images.splice(card1AnswerIndex,0,answerImage);
+        
+        let card2AnswerIndex = Math.floor(Math.random() * card2Images.length + 1);
+        card2Images.splice(card2AnswerIndex,0,answerImage);
+        
+        apendImage($card1,card1Images);
+        apendImage($card2,card2Images);       
+    }
 
-let $card1 = $("#card1");
-let $card2 = $("#card2");
-apendImage($card1,pickUpAndRemoveRandomImages(imageResources));
-apendImage($card2,pickUpAndRemoveRandomImages(imageResources));
+    $('#game-field').on('click','.answer',function(){
+        alert('正解！');
+        $card1.empty();
+        $card2.empty();
+        startGame();
+    });
+
+    startGame();
+
 
