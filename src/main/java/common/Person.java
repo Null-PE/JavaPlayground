8a86089888284e19027752fd093442b989b7007c
ShotaroSuzu <cosmos.1220.3110@gmail.com>
2020-12-23 23:40:26 +0900

    [Transplant]Optional Practice

--- src/main/java/common/Person.java
+++ src/main/java/common/Person.java
@@ -32,4 +32,33 @@ public class Person {
     public void setAge(int age) {
         this.age = age;
     }
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + age;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (age != other.age)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+    
 }
--- src/main/java/optional/EmployeeFactory.java
+++ src/main/java/optional/EmployeeFactory.java
@@ -1,5 +1,4 @@
-package day9;
-
+package optional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -7,18 +6,19 @@ import java.util.List;
 import common.Employee;
 
 public class EmployeeFactory {
-
-    /**
-     * 課題用の社員リストを作る
-     * @return 社員リスト
-     */
-    public static List<Employee> getEmployeeList() {
-        List<Employee> employees = new ArrayList<>();
+	private List<Employee> employees = new ArrayList<>();
+	public EmployeeFactory() {
         employees.add(new Employee("Alice", 22));
         employees.add(new Employee("Bob", 9));
         employees.add(new Employee("Charlie", 17));
         employees.add(new Employee("David", 15));
         employees.add(new Employee("Even", 35));
+	}
+    /**
+     * 課題用の社員リストを作る
+     * @return 社員リスト
+     */
+    public List<Employee> getEmployeeList() {
         return employees;
     }
 
@@ -27,7 +27,7 @@ public class EmployeeFactory {
      * @param name 社員の名前
      * @return 社員インスタンス
      */
-    public static Employee query(String name) {
+    public Employee query(String name) {
         return query(name, 1);
     }
 
@@ -37,7 +37,7 @@ public class EmployeeFactory {
      * @param age 年齢
      * @return 社員インスタンス
      */
-    public static Employee query(String name, int age) {
+    public Employee query(String name, int age) {
         return new Employee(name, age);
     }
 }
