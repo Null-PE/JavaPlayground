f580ee9a3779b6ad98e2d5e16702a23166166eef
flyaway2112 <yamamoto_shu@worksap.co.jp>
2021-01-27 22:07:53 +0900

    add JUnit 5 parameterized test

--- src/test/java/junit/parameterized/SampleEndOfMonthTest.java
+++ src/test/java/junit/parameterized/SampleEndOfMonthTest.java
@@ -1,7 +1,7 @@
 package junit.parameterized;
 
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 import java.util.Arrays;
 import java.util.Calendar;
@@ -15,6 +15,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
+@SuppressWarnings("boxing")
 @RunWith(Enclosed.class)
 public class SampleEndOfMonthTest {
 
@@ -25,6 +26,7 @@ public class SampleEndOfMonthTest {
 		private int month;
 		private int expected;
 		private SampleDayOfMonth sampleDayOfMonth;
+
 		public DayOfMonthTest(int year, int month, int expected) {
 			super();
 			this.year = year;
@@ -36,38 +38,41 @@ public class SampleEndOfMonthTest {
 		@Parameters
 		public static Iterable<Object[]> data() {
 			return Arrays.asList(new Object[][] {
-										{2019, Calendar.JANUARY, 31},
-										{2019, Calendar.FEBRUARY, 28},
-										{2019, Calendar.MARCH, 31},
-										{2019, Calendar.APRIL, 30},
-										{2019, Calendar.MAY, 31},
-										{2019, Calendar.JUNE, 30},
-										{2019, Calendar.JULY, 31},
-										{2019, Calendar.AUGUST, 31},
-										{2019, Calendar.SEPTEMBER, 30},
-										{2019, Calendar.OCTOBER, 31},
-										{2019, Calendar.NOVEMBER, 30},
-										{2019, Calendar.DECEMBER, 31},
-										{2020, Calendar.FEBRUARY, 29},// うるう年
-										{1900, Calendar.FEBRUARY, 28},// うるう年(例外:100年)
-										{2000, Calendar.FEBRUARY, 29},//うるう年(例外の例外:400年)
-										
+					{ 2019, Calendar.JANUARY, 31 },
+					{ 2019, Calendar.FEBRUARY, 28 },
+					{ 2019, Calendar.MARCH, 31 },
+					{ 2019, Calendar.APRIL, 30 },
+					{ 2019, Calendar.MAY, 31 },
+					{ 2019, Calendar.JUNE, 30 },
+					{ 2019, Calendar.JULY, 31 },
+					{ 2019, Calendar.AUGUST, 31 },
+					{ 2019, Calendar.SEPTEMBER, 30 },
+					{ 2019, Calendar.OCTOBER, 31 },
+					{ 2019, Calendar.NOVEMBER, 30 },
+					{ 2019, Calendar.DECEMBER, 31 },
+					{ 2020, Calendar.FEBRUARY, 29 },// うるう年
+					{ 1900, Calendar.FEBRUARY, 28 },// うるう年(例外:100年)
+					{ 2000, Calendar.FEBRUARY, 29 },//うるう年(例外の例外:400年)
+
 			});
 		}
+
 		@Test
 		public void testDayOfMonth() {
 			int day = sampleDayOfMonth.getDayOfMonth(year, month);
 			assertThat(day, is(expected));
 		}
 	}
-	
+
 	//Theories を使ったテスト
 	@RunWith(Theories.class)
 	public static class DayOfMonthTheoriesTest {
 		private SampleDayOfMonth sampleDayOfMonth;
+
 		public DayOfMonthTheoriesTest() {
 			sampleDayOfMonth = new SampleDayOfMonth();
 		}
+
 		@DataPoints
 		public static Fixture[] params = {
 				new Fixture(2019, Calendar.JANUARY, 31),
@@ -85,18 +90,20 @@ public class SampleEndOfMonthTest {
 				new Fixture(2020, Calendar.FEBRUARY, 29),// うるう年
 				new Fixture(1900, Calendar.FEBRUARY, 28),// うるう年(例外:100年)
 				new Fixture(2000, Calendar.FEBRUARY, 29),//うるう年(例外の例外:400年)
-				
+
 		};
+
 		@Theory
 		public void testDayOfMonth(Fixture p) {
 			int day = sampleDayOfMonth.getDayOfMonth(p.year, p.month);
 			assertThat(day, is(p.expected));
 		}
-		
+
 		static class Fixture {
 			int year;
 			int month;
 			int expected;
+
 			public Fixture(int year, int month, int expected) {
 				this.year = year;
 				this.month = month;
